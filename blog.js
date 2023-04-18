e8e35bee275e456c38e05cfbed26afd29c895589
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2023-04-18 12:16:54 +0200

    Added js blog page

---
+++ blog.js
@@ -0,0 +1,32 @@
+const detailContainer = document.querySelector("#js-results");
+
+const url = "http://courseassingment.local/wp-json/wp/v2/posts";
+
+async function fetchresults() {
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+
+    console.log(results);
+
+    makeCards(results);
+  } catch (error) {
+    console.log(error);
+    detailContainer.innerHTML = message("error", error);
+  }
+}
+
+fetchresults();
+
+function makeCards(postsArray) {
+  const cardContainer = document.querySelector("#card-container");
+  console.log(postsArray);
+  postsArray.forEach((post) => {
+    cardContainer.innerHTML += `
+      <a href="details.html?id=${post.id}" class="container">
+        <h2>${post.title.rendered}</h2>
+        <p class="Tilte">Posts:${post.excerpt.rendered}</p>
+      </a>
+    `;
+  });
+}
